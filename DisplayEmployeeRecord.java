0e7371d125f4b560cf15600b4abb6ad41c2962df
Rizwann25 <65345265+Rizwann25@users.noreply.github.com>
2020-09-03 20:10:41 +0530

    Add files via upload

---
+++ DisplayEmployeeRecord.java
@@ -0,0 +1,18 @@
+package employee.arraylist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import java.util.Map.Entry;
+
+public class DisplayEmployeeRecord extends Employee {
+	void displayRecord()
+
+	{
+		ArrayList<String> list = new ArrayList<String>();
+		System.out.println("ID | NAME | DEPARMENT | DESIGNATION");
+		System.out.println("=================================");
+		list.get(id);
+
+	}
+}
